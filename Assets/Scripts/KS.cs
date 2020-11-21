f256f58e38771590ea8d476f41e7bb7b5935267e
Kim Seonghyun <kaltok84@gmail.com>
2020-11-22 00:12:26 +0900

    first commit

---
+++ Assets/Scripts/KS.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KS {
+    private readonly string DEFAULT_FILEPATH = Application.dataPath + "/APPDATA";
+
+    //public static void save(string key, )
+}
