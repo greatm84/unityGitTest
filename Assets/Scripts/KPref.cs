1f0dd7566a13463ef830d1a5d13a5b0b5a884b64
Kim Seonghyun <kaltok84@gmail.com>
2020-11-24 09:43:12 +0900

    more simple

--- Assets/Scripts/KPref.cs
+++ Assets/Scripts/KPref.cs
@@ -5,14 +5,7 @@ using UnityEngine;
 
 public class KPref : BasicDbService {
     private const string DB_NAME = "prefDb.db";
-    private static KPref instance;
-
-    public static KPref Inst() {
-        if (instance == null) {
-            instance = new KPref(DB_NAME);
-        }
-        return instance;
-    }
+    private static KPref instance = new KPref(DB_NAME);    
 
     private KPref(string databaseName) : base(databaseName) {
         Debug.Log("Kpref Con");
@@ -23,61 +16,61 @@ public class KPref : BasicDbService {
         mConnection.CreateTable<KPrefData>();
     }
 
-    public void put(string key, bool data) {
+    public static void put(string key, bool data) {
         put(key, data ? "1" : "0");
     }
 
-    public void put(string key, int data) {
+    public static void put(string key, int data) {
         put(key, data.ToString());
     }
 
-    public void put(string key, float data) {
+    public static void put(string key, float data) {
         put(key, data.ToString());
     }
 
-    public void putObject<T>(string key, T data) {
+    public static void put<T>(string key, T data) {
         put(key, JsonUtility.ToJson(data));
     }
 
-    public void put(string key, string data) {
+    public static void put(string key, string data) {
         instance.mConnection.InsertOrReplace(new KPrefData { Key = key, Data = data });
     }
 
-    public bool? getBoolean(string key) {
+    public static bool? getBoolean(string key) {
         var data = getKeyData(key);
         if (data == null) return null;
         return data.Data == "1";
     }
 
-    public int? getInt(string key) {
+    public static int? getInt(string key) {
         var data = getKeyData(key);
         if (data == null) return null;
         return int.Parse(data.Data);
     }
 
-    public float? getFloat(string key) {
+    public static float? getFloat(string key) {
         var data = getKeyData(key);
         if (data == null) return null;
         return float.Parse(data.Data);
     }
 
-    public string getString(string key) {
+    public static string getString(string key) {
         var data = getKeyData(key);
         if (data == null) return null;
         return data.Data;
     }
 
-    public T getObject<T>(string key) {
+    public static T getObject<T>(string key) {
         var data = getKeyData(key);
         if (data == null) return default(T);
         return JsonUtility.FromJson<T>(data.Data);
     }
 
-    private KPrefData getKeyData(string key) {
+    private static KPrefData getKeyData(string key) {
         return instance.mConnection.Table<KPrefData>().Where(x => x.Key == key).First();
     }
 
-    public bool hasKey(string key) {
+    public static bool hasKey(string key) {
         return getKeyData(key) != null;
     }
 }
--- Assets/Scripts/manmove.cs
+++ Assets/Scripts/manmove.cs
@@ -3,27 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class manmove : MonoBehaviour
-{
+public class manmove : MonoBehaviour {
     InputField inputKey;
     InputField inputData;
     Button btnSave;
 
     InputField inputKey2;
     Button btnQuery;
-    Text txtResult;    
+    Text txtResult;
 
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         findObjects();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
     }
 
     private void findObjects() {
@@ -36,25 +33,51 @@ public class manmove : MonoBehaviour
         txtResult = GameObject.Find("txtResult").GetComponent<Text>();
     }
 
-    public void CallButtonSave() {        
+    public void CallButtonSave() {
         var key = inputKey.text;
-        if(key.Length == 0) {
+        if (key.Length == 0) {
             Debug.Log("no key");
             return;
         }
+        /*
         var data = inputData.text;
 
         KPref.Inst().put(key, data);
         Debug.Log($"save comp {key}, {data}");
+        */
+
+        var data = new People(11, "KSH", 50, 20, 85);
+        KPref.put(key, data);
     }
 
-    public void CallButtonQuery() {        
+    public void CallButtonQuery() {
         var key = inputKey2.text;
         if (key.Length == 0) {
             Debug.Log("no key2");
-            return;        
+            return;
         }
+        /*
         var data = KPref.Inst().getString(key);
         txtResult.text = data;
+        */
+
+        var data = KPref.getObject<People>(key);
+        txtResult.text = data.ToString();
+    }
+
+    class People{
+        public int Id;
+        public string Name;
+        public int Strength;
+        public int Intelligent;
+        public int Dex;
+
+        public People(int id, string name, int strength, int intelligent, int dex) {
+            Id = id;
+            Name = name;
+            Strength = strength;
+            Intelligent = intelligent;
+            Dex = dex;
+        }
     }
 }
